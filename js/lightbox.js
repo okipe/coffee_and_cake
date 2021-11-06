928fd40e947e91d60e649700ac66d2b01c850d6e
Oscar Roman <oscarroman.pe@gmail.com>
2021-11-06 01:33:54 -0500

    Lightbox effect for photos

--- js/lightbox.js
+++ js/lightbox.js
@@ -3,7 +3,12 @@
 var propLightbox = {
     imgContainer: document.getElementsByClassName('lightbox'),
     imagen: null,
-    imagenSrc: null
+    imagenSrc: null,
+    cuerpoDom: document.getElementsByTagName('body')[0],
+    lightbox_container: null,
+    modal: null,
+    cerrarModal: null,
+    animacion: 'fade'
 }
 
 var metLightbox = {
@@ -20,7 +25,39 @@ var metLightbox = {
 
     lightbox: function(imagen){
         propLightbox.imagenSrc = window.getComputedStyle(imagen, null).backgroundImage.slice(5, -2);
-        console.log(propLightbox.imagenSrc);
+        propLightbox.cuerpoDom.appendChild(document.createElement('DIV')).setAttribute('id', 'lightbox_container');
+        propLightbox.lightbox_container = document.getElementById('lightbox_container');
+        propLightbox.lightbox_container.style.width = '100%';
+        propLightbox.lightbox_container.style.height = '100%';
+        propLightbox.lightbox_container.style.position = 'fixed';
+        propLightbox.lightbox_container.style.zIndex = '1000';
+        propLightbox.lightbox_container.style.background = 'rgba(0,0,0,0.8)';
+        propLightbox.lightbox_container.style.top = '0';
+        propLightbox.lightbox_container.style.left = '0';
+
+        propLightbox.lightbox_container.appendChild(document.createElement('DIV')).setAttribute('id', 'modal');
+        propLightbox.modal = document.getElementById('modal');
+        
+        propLightbox.modal.style.height = '100%';
+        
+        propLightbox.modal.appendChild(document.createElement('IMG')).setAttribute('src', propLightbox.imagenSrc);
+        propLightbox.modal.getElementsByTagName('img')[0].setAttribute('class', 'imagen-modal');
+
+        if (propLightbox.animacion == 'fade') {
+            document.getElementsByClassName('imagen-modal')[0].style.opacity = 0;
+            setTimeout(function() {
+                document.getElementsByClassName('imagen-modal')[0].style.opacity = 1;
+            }, 50);
+        }
+
+        propLightbox.modal.innerHTML += '<i id="cerrar_modal" class="fa fa-times" aria-hidden="true"></i>';
+        propLightbox.cerrarModal = document.getElementById('cerrar_modal');
+        propLightbox.cerrarModal.addEventListener('click', metLightbox.cerrarModal);
+
+    },
+
+    cerrarModal: function(){
+        propLightbox.cuerpoDom.removeChild(propLightbox.lightbox_container); 
     }
 }
 
